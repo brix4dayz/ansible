4f3a3cf2ac1e4fb2896a04625e2742b185cf9fc8
Robert Benson <rbenson@bandwidth.com>
2018-10-01 22:45:54 -0400

    check for None on get_by_name (#1)
    * check for None on get_by_name
    
    * actually fix get_by_name, also last commit fixed get_by_id

--- lib/ansible/modules/cloud/sysdig/sysdig_alert.py
+++ lib/ansible/modules/cloud/sysdig/sysdig_alert.py
@@ -234,11 +234,11 @@ class SysDigObject(object):
 
     def _get_by_id(self, sysdig_id):
         _result = self.client.get_by_id(self.resource, sysdig_id)
-        return _result['id'], _result
+        return (_result['id'], _result) if _result is not None else (None, None)
 
     def _get_by_name(self, name):
         _result = self.client.get_by_name(self.resource, name)
-        return _result['id'], _result
+        return (_result['id'], _result) if _result is not None else (None, None)
 
     def create(self):
         if self.exists:
